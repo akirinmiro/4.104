bd318f96178efc2014ff8e552304d34756c6b74c
Сhert <hert@AKIRIN>
2023-01-16 11:14:58 +0300

    Добавьте файлы проекта.

---
+++ 4.104/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+    public enum DayOfWeek
+    {
+        Понедельник = 0,
+        Вторник = 1,
+        Среда = 2,
+        Четверг = 3,
+        Пятниццо = 4,
+        Суббота = 5,
+        Воскресенье = 6
+    }
+    static void Main(string[] args)
+    {
+        Console.Write("Введите номер дня недели: ");
+        var n = int.Parse(Console.ReadLine());
+        Console.WriteLine((DayOfWeek)((n - 1) % Enum.GetValues(typeof(DayOfWeek)).Length));
+    }
+
+
+
+
+
+}
+    
+
